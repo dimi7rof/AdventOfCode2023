67a39b5ac18c711dfc0e29a1c23ff8b800ed0b6d
dimi7 <dimi7@DESKTOP-2UM0VJE>
2023-12-28 11:46:37 +0200

    day1 part2

--- AOC2023a/Day01.cs
+++ AOC2023a/Day01.cs
@@ -1,75 +1,43 @@
-using System.Text.RegularExpressions;
-
 namespace AOC2023a;
 
 public static class Day01
 {
-    public static void Day01_2()
+    public static int Part01()
     {
-        var sum =0;
         var input = File.ReadAllLines("..\\..\\..\\inputDay01.txt");
-        var input2 = new List<string>() {
-            "two1nine",
-            "eightwothree",
-            "abcone2threexyz",
-            "xtwone3four",
-            "4nineeightseven2",
-            "zoneight234",
-            "7pqrstsixteen"
-        };
-        var digitAsString = new Dictionary<string, int>() {
-            { "one", 1 },
-            { "two", 2 },
-            { "three", 3},
-            { "four", 4 },
-            { "five", 5 },
-            { "six", 6 },
-            { "seven", 7 },
-            { "eight", 8 },
-            { "nine", 9 }
-        };
-        foreach (var line in input2) 
-        {
-            var newLine = line;
-            foreach (var (key,value) in digitAsString)
-            {
-                if(line.Contains(key))
-                {
-                    //newLine = line.Replace(key, value);
-                    //line.Remove(line.IndexOf(key)).Replace(key, value);
-                }
-            }
-            //if(line.Contains(test.Any()))
-            var digits = line.Where(Char.IsDigit).ToArray();
-            if(digits.Length == 1) 
-            {
-                sum +=Convert.ToInt32(new string([digits[0],digits[0]]));
-            }
-            if(digits.Length >= 2) 
-            {
-                sum += Convert.ToInt32(new string([digits[0],digits[^1]]));
-            }
-        }
+
+        return CountDigits(input);
     }
 
-    public static void Day01_1()
+    public static int Part02()
+    {
+        var input = File.ReadAllText("..\\..\\..\\inputDay01.txt");
+
+        var repalced = input
+            .Replace("one", "o1e")
+            .Replace("two", "t2o")
+            .Replace("three", "thr3e")
+            .Replace("four", "fo4r")
+            .Replace("five", "fi5e")
+            .Replace("six", "s6x")
+            .Replace("seven", "se7en")
+            .Replace("eight", "ei8ht")
+            .Replace("nine", "n9ne");
+
+        return CountDigits(repalced.Split(Environment.NewLine));
+    }
+
+    private static int CountDigits(string[] input)
     {
         var sum = 0;
-        var input = File.ReadAllLines("..\\..\\..\\inputDay01.txt");
 
-        foreach (var line in input) 
+        foreach (var line in input)
         {
             var digits = line.Where(Char.IsDigit).ToArray();
-            if(digits.Length == 1) 
-            {
-                sum +=Convert.ToInt32(new string([digits[0],digits[0]]));
-            }
-            if(digits.Length >= 2) 
-            {
-                sum += Convert.ToInt32(new string([digits[0],digits[^1]]));
-            }
+            sum += Convert.ToInt32(new string([digits[0], digits[^1]]));
         }
-        Console.WriteLine(sum);
+
+        return sum;
     }
 }
 
